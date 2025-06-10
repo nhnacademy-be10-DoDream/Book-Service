03b515289bbb3964c17481832506cac1fdca5266
MonaLisaSy <ugs628@naver.com>
2025-06-10 11:22:11 +0900

    feat: add dto
    dto 추가한다.
    
    #5

---
+++ src/main/java/shop/dodream/book/dto/CategoryRequest.java
@@ -0,0 +1,15 @@
+package shop.dodream.book.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+public class CategoryRequest {
+    private String categoryName;
+    private Long depth;
+}
