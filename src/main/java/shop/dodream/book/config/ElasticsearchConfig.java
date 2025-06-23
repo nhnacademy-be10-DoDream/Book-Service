f8c15914dda66a13e6c24cc00a6eae5c94792510
song-sang-jin <ssj30099@naver.com>
2025-06-23 15:36:49 +0900

    feat: elastic config 설정
    #43

---
+++ src/main/java/shop/dodream/book/config/ElasticsearchConfig.java
@@ -0,0 +1,31 @@
+package shop.dodream.book.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+
+
+@Configuration
+@EnableElasticsearchRepositories
+public class ElasticsearchConfig extends ElasticsearchConfiguration {
+
+
+    @Value("${elasticsearch.uris}")
+    private String host;
+
+    @Value("${elasticsearch.username}")
+    private String username;
+
+    @Value("${elasticsearch.password}")
+    private String password;
+
+    @Override
+    public ClientConfiguration clientConfiguration() {
+        return ClientConfiguration.builder()
+                .connectedTo(host)
+                .withBasicAuth(username,password)
+                .build();
+    }
+}
