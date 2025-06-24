c156a107fa9853014b3e845280095eb31693d28c
chosun-nhn17 <12160569@inha.edu>
2025-06-24 21:28:27 +0900

    fix: elasticsearchconfig exclude ci profile
    #54

--- src/main/java/shop/dodream/book/config/ElasticsearchConfig.java
+++ src/main/java/shop/dodream/book/config/ElasticsearchConfig.java
@@ -7,6 +7,7 @@ import org.elasticsearch.client.RestClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
@@ -14,6 +15,7 @@ import org.springframework.data.elasticsearch.repository.config.EnableElasticsea
 
 @Configuration
 @EnableElasticsearchRepositories
+@Profile("!ci")
 public class ElasticsearchConfig extends ElasticsearchConfiguration {
 
 
