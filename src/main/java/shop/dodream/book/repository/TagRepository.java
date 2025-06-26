185884b1ba5e4cd789a33a10e9415839510f8285
MonaLisaSy <ugs628@naver.com>
2025-06-26 14:10:24 +0900

    refactor: category,tag repository 리팩토링
    #81

--- src/main/java/shop/dodream/book/repository/TagRepository.java
+++ src/main/java/shop/dodream/book/repository/TagRepository.java
@@ -1,13 +1,10 @@
 package shop.dodream.book.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import shop.dodream.book.entity.Tag;
 
-import java.util.List;
-
 @Repository
 public interface TagRepository extends JpaRepository<Tag, Long> {
+    boolean existsById(Long tagId);
 }
