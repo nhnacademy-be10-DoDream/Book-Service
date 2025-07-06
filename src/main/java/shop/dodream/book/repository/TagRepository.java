b78a1774cac65df408f8cad66090a7ba351797e2
MonaLisaSy <ugs628@naver.com>
2025-07-06 18:10:14 +0900

    refactor: register and select modify
    #121

--- src/main/java/shop/dodream/book/repository/TagRepository.java
+++ src/main/java/shop/dodream/book/repository/TagRepository.java
@@ -5,9 +5,11 @@ import org.springframework.stereotype.Repository;
 import shop.dodream.book.entity.Tag;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface TagRepository extends JpaRepository<Tag, Long> {
     boolean existsById(Long tagId);
+    Optional<Tag> findById(Long tagId);
     List<Tag> findAll();
 }
