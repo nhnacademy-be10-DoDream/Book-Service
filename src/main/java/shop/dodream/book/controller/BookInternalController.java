a737e8d102c8279ad76acaa7048694e7184bc8a9
song-sang-jin <ssj30099@naver.com>
2025-07-10 17:45:54 +0900

    feat: 주문 환불시 재고 반납하는 API 추가
    #160

--- src/main/java/shop/dodream/book/controller/BookInternalController.java
+++ src/main/java/shop/dodream/book/controller/BookInternalController.java
@@ -4,13 +4,12 @@ package shop.dodream.book.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import shop.dodream.book.dto.BookCountDecreaseRequest;
 import shop.dodream.book.dto.BookCountDecreaseResponse;
+import shop.dodream.book.dto.BookCountIncreaseRequest;
 import shop.dodream.book.service.BookService;
 
 
@@ -26,6 +25,14 @@ public class BookInternalController {
     public BookCountDecreaseResponse decreaseBookCount(@Validated @RequestBody BookCountDecreaseRequest request){
 
         return bookService.decreaseBookCount(request);
+    }
+
+    @Operation(summary = "도서 재고 추가", description = "환불 시 도서 재고를 증가하는 API입니다.")
+    @PostMapping("/increase-bookCount")
+    public ResponseEntity<Void> increaseBookCount(@Validated @RequestBody BookCountIncreaseRequest request){
+        bookService.increaseBookCount(request);
+
+        return ResponseEntity.noContent().build();
     }
 
 }
