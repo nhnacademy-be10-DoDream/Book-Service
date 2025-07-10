daeedf78e87b4f324349fb0ffefceb4beaa48a0f
song-sang-jin <ssj30099@naver.com>
2025-07-10 15:57:57 +0900

    feat: 책 사용자 상세조회 reviewCount 증가 redis 로 담아놧다가 한꺼번에 저장
    #159

---
+++ src/main/java/shop/dodream/book/config/RedisConfig.java
@@ -0,0 +1,13 @@
+//package shop.dodream.book.config;
+//
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.data.redis.core.RedisTemplate;
+//
+//@Configuration
+//public class RedisConfig {
+//
+//    @Bean
+//    public RedisTemplate<String, Object>
+//}
