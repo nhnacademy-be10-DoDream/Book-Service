3abbdc756ee1196b553ab4a31a414488a653b033
song-sang-jin <ssj30099@naver.com>
2025-07-15 10:40:06 +0900

    refactor: 책 검색시 삭제된도서 제외 필터링 추가
    #174

--- src/main/java/shop/dodream/book/service/impl/BookSearchServiceImpl.java
+++ src/main/java/shop/dodream/book/service/impl/BookSearchServiceImpl.java
@@ -2,6 +2,7 @@ package shop.dodream.book.service.impl;
 
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
 import co.elastic.clients.elasticsearch._types.SortOptions;
+import co.elastic.clients.elasticsearch._types.query_dsl.BoolQuery;
 import co.elastic.clients.elasticsearch._types.query_dsl.MultiMatchQuery;
 import co.elastic.clients.elasticsearch._types.query_dsl.Query;
 import co.elastic.clients.elasticsearch.core.SearchRequest;
@@ -31,57 +32,48 @@ public class BookSearchServiceImpl implements BookSearchService {
     @Override
     public Page<BookItemResponse> searchBooks(String keyword, BookSortType sortType, Pageable pageable) {
         try {
-            Query query;
-
             MultiMatchQuery multiMatchQuery = new MultiMatchQuery.Builder()
                     .query(keyword)
                     .fields("title^100", "author^50", "categoryNames^10", "description^5")
                     .build();
 
+            BoolQuery.Builder boolQueryBuilder = new BoolQuery.Builder()
+                    .must(m -> m.multiMatch(multiMatchQuery)) // keyword 검색 조건
+                    .mustNot(mn -> mn.term(t -> t.field("status").value("REMOVED"))); // status=REMOVED 제외 조건
+
             if (sortType == BookSortType.RATING) {
-                query = new Query.Builder()
-                        .bool(b -> b
-                                .must(m -> m.multiMatch(multiMatchQuery))
-                                .filter(f -> f.range(r -> r
-                                        .field("reviewCount")
-                                        .gte(JsonData.of(100))
-                                ))
-                        )
-                        .build();
-            } else {
-                query = new Query.Builder()
-                        .multiMatch(multiMatchQuery)
-                        .build();
+                boolQueryBuilder.filter(f -> f.range(r -> r
+                        .field("reviewCount")
+                        .gte(JsonData.of(100))
+                ));
             }
 
+            Query query = new Query.Builder()
+                    .bool(boolQueryBuilder.build())
+                    .build();
 
             SortOptions sortOption = sortType.toSortOption();
 
             SearchRequest request = new SearchRequest.Builder()
                     .index("dodream_books")
                     .query(query)
-                    .from((int)pageable.getOffset()) // offset = page * size
+                    .from((int) pageable.getOffset())  // offset = page * size
                     .size(pageable.getPageSize())
                     .sort(sortOption != null ? Collections.singletonList(sortOption) : Collections.emptyList())
                     .build();
 
-            SearchResponse<BookDocument> response = elasticsearchClient.search(
-                    request,
-                    BookDocument.class
-            );
+            SearchResponse<BookDocument> response = elasticsearchClient.search(request, BookDocument.class);
 
             List<BookItemResponse> content = response.hits().hits().stream()
                     .map(Hit::source)
                     .map(BookItemResponse::new)
                     .toList();
 
-
-            long totalHits = response.hits().total() != null ? response.hits().total().value() : content.size();
-
+            long totalHits = response.hits().total() != null
+                    ? response.hits().total().value()
+                    : content.size();
 
             return new PageImpl<>(content, pageable, totalHits);
-
-            
 
         } catch (IOException e) {
             e.printStackTrace();
