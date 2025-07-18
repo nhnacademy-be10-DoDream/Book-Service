77b1c651aa0b1cd64e5de3ae4b46c83989eda84e
MonaLisaSy <163082700+MonaLisaSy@users.noreply.github.com>
2025-07-18 11:19:16 +0900

    [RELEASE] 4.4.1차 배포  (#197)
    * merge: feat/191-elastic-filter into develop (#192)
    
    #191
    
    * merge: feat/195-search-counter into develop (#196)
    
    * feat: category elastic category, price
    
    #191
    
    * feat: category elastic counter
    
    #195

--- src/main/java/shop/dodream/book/controller/BookSearchController.java
+++ src/main/java/shop/dodream/book/controller/BookSearchController.java
@@ -3,13 +3,12 @@ package shop.dodream.book.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import shop.dodream.book.dto.BookItemResponse;
+import shop.dodream.book.dto.BookItemWithCountResponse;
 import shop.dodream.book.dto.BookSortType;
 import shop.dodream.book.service.BookSearchService;
 
@@ -25,11 +24,13 @@ public class BookSearchController {
 
     @Operation(summary = "도서 검색", description = "키워드를 통해 도서를 검색합니다. 정렬 옵션을 선택할 수 있습니다.")
     @GetMapping("/search")
-    public Page<BookItemResponse> searchBooks(@RequestParam String keyword,
-                                              @RequestParam(value = "sort", required = false, defaultValue = "NONE") BookSortType sort, Pageable pageable,
-                                              @RequestParam(value = "categoryIds", required = false) List<Long> categoryIds,
-                                              @RequestParam(value = "minPrice", required = false) Integer minPrice,
-                                              @RequestParam(value = "maxPrice", required = false) Integer maxPrice) {
+
+    public BookItemWithCountResponse searchBooks(@RequestParam String keyword,
+                                                 @RequestParam(value = "sort", required = false, defaultValue = "NONE") BookSortType sort,
+                                                 Pageable pageable,
+                                                 @RequestParam(value = "categoryIds", required = false) List<Long> categoryIds,
+                                                 @RequestParam(value = "minPrice", required = false) Integer minPrice,
+                                                 @RequestParam(value = "maxPrice", required = false) Integer maxPrice) {
         return bookSearchService.searchBooks(keyword, sort, pageable, categoryIds, minPrice, maxPrice);
     }
 }
